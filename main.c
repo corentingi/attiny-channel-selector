69453b04e82ae60e940ea548387db6d339afc6cf
Corentin Gitton <corentin.gitton@gmail.com>
2019-08-10 18:59:29 +0200

    Implement basic logic working with LEDs as display

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
-#define F_CPU 1000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-uint8_t i = 0;                   // counter variable
-uint8_t previousReadingPB3 = 1;  // holds last 8 pin reads from pushbutton
-uint8_t buttonWasPressedPB3 = 1; // keeps track of debounced button state, 0 false, 1 true
-uint8_t previousReadingPB4 = 1;  // holds last 8 pin reads from pushbutton
-uint8_t buttonWasPressedPB4 = 1; // keeps track of debounced button state, 0 false, 1 true
+volatile uint8_t selectedChannel = 0;     // channel selection (counter)
+volatile uint8_t previousState = 0;
 
+// Intialize TIMER1
 static inline void initTimer1(void)
 {
     TCCR1 |= (1 << CTC1);                             // clear timer on compare match
@@ -17,41 +14,47 @@ static inline void initTimer1(void)
     sei();                                            // enable interrupts
 }
 
+// Debounce function
+unsigned char debounce(unsigned char sample)
+{
+    static unsigned char state, cnt0, cnt1;
+    unsigned char delta;
+
+    delta = sample ^ state;
+    cnt1 = (cnt1 ^ cnt0) & (delta & sample);
+    cnt0 = ~cnt0 & (delta & sample);
+    state ^= (delta & ~(cnt0 | cnt1));
+
+    return state;
+}
+
+// Interrupt function for TIMER1
 ISR(TIMER1_COMPA_vect)
 {
-    // PB3
-    if ((PINB & (1 << PB3)) != previousReadingPB3) // if current button pin reading doesn't equal previousReadingPB3 200us ago,
-    {
-        if (!buttonWasPressedPB3) // and button wasn't pressed last time
-        {
-            if (i < 7) // increment i from 0 to 7
-                i++;
-            else
-                i = 0;
-            PORTB = (PORTB & 0b11111000) | (i & 0b00000111); // Set PORTB to i with a mask
-            buttonWasPressedPB3 = 1;                         // set debounced button press state to 1
-        }
-        else
-            buttonWasPressedPB3 = 0; // button has been let go, reset buttonWasPressed
-    }
-    previousReadingPB3 = (PINB & (1 << PB3)); // set previous reading to current reading for next time
+    // Debounced pin state
+    uint8_t state = debounce(PINB);
 
-    // PB4
-    if ((PINB & (1 << PB4)) != previousReadingPB4) // if current button pin reading doesn't equal previousReadingPB4 200us ago,
+    // If PB3 was just released (rising edge with INPUT_PULLUP)
+    // PB3 increment selected channel
+    if ( (state & (1 << PB3)) && !(previousState & (1 << PB3)) )
     {
-        if (!buttonWasPressedPB4) // and button wasn't pressed last time
-        {
-            if (i > 0) // decrement i from 7 to 0
-                i--;
-            else
-                i = 7;
-            PORTB = (PORTB & 0b11111000) | (i & 0b00000111); // Set PORTB to i with a mask
-            buttonWasPressedPB4 = 1;                         // set debounced button press state to 1
-        }
-        else
-            buttonWasPressedPB4 = 0; // button has been let go, reset buttonWasPressed
+        if (selectedChannel < 7)
+            selectedChannel++;
     }
-    previousReadingPB4 = (PINB & (1 << PB4)); // set previous reading to current reading for next time
+
+    // If PB4 was just released (rising edge with INPUT_PULLUP)
+    // PB4 decrement selected channel
+    if ( (state & (1 << PB4)) && !(previousState & (1 << PB4)) )
+    {
+        if (selectedChannel > 0)
+            selectedChannel--;
+    }
+
+    if (state != previousState)
+        PORTB = (PORTB & 0b11111000) | (selectedChannel & 0b00000111); // Set PORTB to i with a mask
+
+    // Save current state for future comparision
+    previousState = state;
 }
 
 int main(void)
